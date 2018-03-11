01712400e690fd382a0dd5fd465b83447e7874d5
Milad Irannejad <moorara@gmail.com>
2018-03-10 21:56:51 -0500

    Update packages

--- services/react-client/src/reducers/index.js
+++ services/react-client/src/reducers/index.js
@@ -3,12 +3,12 @@ import { combineReducers } from 'redux'
 import siteReducer, { defaultState as siteDefaultState } from './site'
 import sensorReducer, { defaultState as sensorDefaultState } from './sensor'
 
-export default combineReducers({
-  site: siteReducer,
-  sensor: sensorReducer,
-})
-
 export const defaultState = {
   site: siteDefaultState,
   sensor: sensorDefaultState
 }
+
+export default combineReducers({
+  site: siteReducer,
+  sensor: sensorReducer,
+})
