8141755de6957f2f259efc0d6f3faab31579812c
Milad Irannejad <moorara@users.noreply.github.com>
2018-03-18 22:09:12 -0400

    Fix dev server (#25)

--- services/react-client/server/dev/dev.js
+++ services/react-client/server/dev/dev.js
@@ -3,46 +3,47 @@
  */
 
 import 'colors'
-import _ from 'lodash'
 import opn from 'opn'
+import path from 'path'
 import express from 'express'
 import webpack from 'webpack'
-import jsonServer from 'json-server'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 
-import mockData from './api/data'
+import mockApiServer from './api'
 import webpackConfig from '../../webpack.dev'
 
 const port = parseInt(process.env.PORT, 10) || 4000
-const hmr = 'webpack-hot-middleware/client?path=/webpack_hmr&timeout=2000'
+
+// Update config for hot reloading
+const hmr = 'webpack-hot-middleware/client?path=/hmr&timeout=2000&overlay=true'
+webpackConfig.entry.app = [ hmr, webpackConfig.entry.app ]
 
 const app = express()
 const transpiler = webpack(webpackConfig)
 
-// Use json-server for mocking api backend
-const apiRouter = jsonServer.router(mockData)
-const apiMiddleware = jsonServer.defaults()
-
-// Update config for hot reloading
-if (_.isArray(webpackConfig.entry.app)) {
-  webpackConfig.entry.app.unshift(hmr)
-} else if (_.isString(webpackConfig.entry.app)) {
-  webpackConfig.entry.app = [ hmr, webpackConfig.entry.app ]
-}
-
-app.use(webpackDevMiddleware(transpiler, {
+const devMiddleware = webpackDevMiddleware(transpiler, {
   noInfo: true,
   publicPath: webpackConfig.output.publicPath
-}))
+})
 
-app.use(webpackHotMiddleware(transpiler, {
-  path: '/webpack_hmr',
+const hotMiddleware = webpackHotMiddleware(transpiler, {
+  path: '/hmr',
   heartbeat: 1000
-}))
+})
 
-// Api mock server
-app.use('/api/v1', apiMiddleware, apiRouter)
+// Respond with client on every path
+const filename = path.join(transpiler.outputPath, 'index.html')
+const allMiddleware = (req, res) => {
+  devMiddleware.fileSystem.readFile(filename, (err, file) => {
+    err ? res.sendStatus(404) : res.send(file.toString())
+  })
+}
+
+mockApiServer(app)
+app.use(devMiddleware)
+app.use(hotMiddleware)
+app.use(allMiddleware)
 
 app.listen(port, err => {
   if (err) {
