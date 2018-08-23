2d0c3d8d25f4e1d35a383405fda0d4a8d698d368
Milad Irannejad <moorara@gmail.com>
2018-08-23 12:52:56 -0400

    site-server: cross-service tracing

--- services/site-service/middleware/tracer.js
+++ services/site-service/middleware/tracer.js
@@ -1,4 +1,5 @@
 const onFinished = require('on-finished')
+const opentracing = require('opentracing')
 
 const { createTracer } = require('../util/tracer')
 
@@ -7,25 +8,21 @@ module.exports.create = options => {
   options.tracer = options.tracer || createTracer({ serviceName: 'middleware' })
 
   return (req, res, next) => {
-    const span = options.tracer.startSpan('http-request')
+    // https://opentracing-javascript.surge.sh
+    const parentSpanContext = options.tracer.extract(opentracing.FORMAT_HTTP_HEADERS, req.headers)
+    const span = options.tracer.startSpan('http-request', { childOf: parentSpanContext })
     req.context = { span }
 
     onFinished(res, (err, res) => {
       if (err) {
-        return
+        span.log({ message: err.message })
       }
 
       // https://github.com/opentracing/specification/blob/master/semantic_conventions.md
-      span.setTag('http.method', req.method)
-      span.setTag('http.url', req.originalUrl)
-      span.setTag('http.status_code', res.statusCode)
-      span.log({
-        event: 'http-request',
-        method: req.method,
-        url: req.originalUrl,
-        statusCode: res.statusCode
-      })
-
+      span.setTag('http.version', req.httpVersion)
+      span.setTag(opentracing.Tags.HTTP_METHOD, req.method)
+      span.setTag(opentracing.Tags.HTTP_URL, req.originalUrl)
+      span.setTag(opentracing.Tags.HTTP_STATUS_CODE, res.statusCode)
       span.finish()
     })
 
