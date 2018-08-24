2edb950ffb3d4a209207fda3c004ddbbefa7edeb
Milad Irannejad <moorara@gmail.com>
2018-08-23 23:20:50 -0400

    sensor-server: cross-service tracing

--- services/sensor-service/middleware/tracer.go
+++ services/sensor-service/middleware/tracer.go
@@ -5,8 +5,10 @@ import (
 
 	"github.com/moorara/microservices-demo/services/sensor-service/util"
 	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/log"
+	"github.com/opentracing/opentracing-go/ext"
 )
+
+const spanName = "http-request"
 
 type tracerMiddleware struct {
 	tracer opentracing.Tracer
@@ -21,28 +23,31 @@ func NewTracerMiddleware(tracer opentracing.Tracer) Middleware {
 
 func (m *tracerMiddleware) Wrap(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		span := m.tracer.StartSpan("http-request")
-		ctx := opentracing.ContextWithSpan(r.Context(), span)
+		var span opentracing.Span
 
+		carrier := opentracing.HTTPHeadersCarrier(r.Header)
+		parentSpanContext, err := m.tracer.Extract(opentracing.HTTPHeaders, carrier)
+		if err != nil {
+			span = m.tracer.StartSpan(spanName)
+		} else {
+			span = m.tracer.StartSpan(spanName, opentracing.ChildOf(parentSpanContext))
+		}
+
+		ctx := opentracing.ContextWithSpan(r.Context(), span)
 		rw := util.NewResponseWriter(w)
 		req := r.WithContext(ctx)
 		next(rw, req)
 
+		proto := r.Proto
 		method := r.Method
 		url := r.URL.Path
-		statusCode := rw.StatusCode()
+		statusCode := uint16(rw.StatusCode())
 
 		// https://github.com/opentracing/specification/blob/master/semantic_conventions.md
-		span.SetTag("http.method", method)
-		span.SetTag("http.url", url)
-		span.SetTag("http.status_code", statusCode)
-		span.LogFields(
-			log.String("event", "http-request"),
-			log.String("method", method),
-			log.String("url", url),
-			log.Int("statusCode", statusCode),
-		)
-
+		span.SetTag("http.version", proto)
+		ext.HTTPMethod.Set(span, method)
+		ext.HTTPUrl.Set(span, url)
+		ext.HTTPStatusCode.Set(span, statusCode)
 		span.Finish()
 	}
 }
--- services/sensor-service/service/sensor.go
+++ services/sensor-service/service/sensor.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-kit/kit/log/level"
 	"github.com/google/uuid"
 	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
 
 	opentracingLog "github.com/opentracing/opentracing-go/log"
 )
@@ -48,6 +49,30 @@ func NewSensorManager(db DB, logger log.Logger, tracer opentracing.Tracer) Senso
 	}
 }
 
+func (m *postgresSensorManager) exec(ctx context.Context, name, query string, fn func() error) error {
+	parentSpan := opentracing.SpanFromContext(ctx)
+	span := m.tracer.StartSpan(name, opentracing.ChildOf(parentSpan.Context()))
+	defer span.Finish()
+
+	// https://github.com/opentracing/specification/blob/master/semantic_conventions.md
+	ext.DBType.Set(span, "sql")
+	ext.DBStatement.Set(span, query)
+
+	err := fn()
+	if err != nil {
+		level.Error(m.logger).Log("message", err.Error())
+	}
+
+	span.LogFields(opentracingLog.String("event", name))
+	if err == nil {
+		span.LogFields(opentracingLog.String("message", "successful!"))
+	} else {
+		span.LogFields(opentracingLog.String("message", err.Error()))
+	}
+
+	return err
+}
+
 func (m *postgresSensorManager) Create(ctx context.Context, siteID, name, unit string, minSafe, maxSafe float64) (*Sensor, error) {
 	sensor := &Sensor{
 		ID:      uuid.New().String(),
@@ -57,129 +82,93 @@ func (m *postgresSensorManager) Create(ctx context.Context, siteID, name, unit s
 		MinSafe: minSafe,
 		MaxSafe: maxSafe,
 	}
-
-	parentSpan := opentracing.SpanFromContext(ctx)
-	span := m.tracer.StartSpan("insert-record", opentracing.ChildOf(parentSpan.Context()))
-	defer span.Finish()
-
 	query := `insert into sensors (id, site_id, name, unit, min_safe, max_safe) values ($1, $2, $3, $4, $5, $6)`
-	_, err := m.db.ExecContext(ctx, query, sensor.ID, sensor.SiteID, sensor.Name, sensor.Unit, sensor.MinSafe, sensor.MaxSafe)
+
+	err := m.exec(ctx, "insert-record", query, func() error {
+		_, err := m.db.ExecContext(ctx, query, sensor.ID, sensor.SiteID, sensor.Name, sensor.Unit, sensor.MinSafe, sensor.MaxSafe)
+		return err
+	})
+
 	if err != nil {
-		level.Error(m.logger).Log("message", err.Error())
 		return nil, err
 	}
-
-	span.SetTag("db.type", "sql")
-	span.LogFields(
-		opentracingLog.String("event", "insert-record"),
-		opentracingLog.String("db.statement", query),
-	)
-
 	return sensor, nil
 }
 
 func (m *postgresSensorManager) All(ctx context.Context, siteID string) ([]Sensor, error) {
 	sensors := make([]Sensor, 0)
-
-	parentSpan := opentracing.SpanFromContext(ctx)
-	span := m.tracer.StartSpan("select-records", opentracing.ChildOf(parentSpan.Context()))
-	defer span.Finish()
-
 	query := `select id, site_id, name, unit, min_safe, max_safe from sensors where site_id = $1`
-	rows, err := m.db.QueryContext(ctx, query, siteID)
+
+	err := m.exec(ctx, "select-records", query, func() error {
+		rows, err := m.db.QueryContext(ctx, query, siteID)
+		if err != nil {
+			return err
+		}
+
+		for rows.Next() {
+			sensor := Sensor{}
+			err := rows.Scan(&sensor.ID, &sensor.SiteID, &sensor.Name, &sensor.Unit, &sensor.MinSafe, &sensor.MaxSafe)
+			if err == nil {
+				sensors = append(sensors, sensor)
+			}
+		}
+
+		return nil
+	})
+
 	if err != nil {
-		level.Error(m.logger).Log("message", err.Error())
 		return nil, err
 	}
-
-	span.SetTag("db.type", "sql")
-	span.LogFields(
-		opentracingLog.String("event", "select-records"),
-		opentracingLog.String("db.statement", query),
-	)
-
-	for rows.Next() {
-		sensor := Sensor{}
-		err := rows.Scan(&sensor.ID, &sensor.SiteID, &sensor.Name, &sensor.Unit, &sensor.MinSafe, &sensor.MaxSafe)
-		if err == nil {
-			sensors = append(sensors, sensor)
-		}
-	}
-
 	return sensors, nil
 }
 
 func (m *postgresSensorManager) Get(ctx context.Context, id string) (*Sensor, error) {
 	sensor := new(Sensor)
-
-	parentSpan := opentracing.SpanFromContext(ctx)
-	span := m.tracer.StartSpan("select-record", opentracing.ChildOf(parentSpan.Context()))
-	defer span.Finish()
-
 	query := `select id, site_id, name, unit, min_safe, max_safe from sensors where id = $1`
-	row := m.db.QueryRowContext(ctx, query, id)
-	err := row.Scan(&sensor.ID, &sensor.SiteID, &sensor.Name, &sensor.Unit, &sensor.MinSafe, &sensor.MaxSafe)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, nil
+
+	err := m.exec(ctx, "select-record", query, func() error {
+		row := m.db.QueryRowContext(ctx, query, id)
+		err := row.Scan(&sensor.ID, &sensor.SiteID, &sensor.Name, &sensor.Unit, &sensor.MinSafe, &sensor.MaxSafe)
+		if err == sql.ErrNoRows { // record does not exist
+			sensor = nil
+			return nil
 		}
-		level.Error(m.logger).Log("message", err.Error())
+		return err
+	})
+
+	if err != nil {
 		return nil, err
 	}
-
-	span.SetTag("db.type", "sql")
-	span.LogFields(
-		opentracingLog.String("event", "select-record"),
-		opentracingLog.String("db.statement", query),
-	)
-
 	return sensor, nil
 }
 
 func (m *postgresSensorManager) Update(ctx context.Context, s Sensor) (int, error) {
-	parentSpan := opentracing.SpanFromContext(ctx)
-	span := m.tracer.StartSpan("update-record", opentracing.ChildOf(parentSpan.Context()))
-	defer span.Finish()
-
+	var n int64
 	query := `update sensors set site_id = $2, name = $3, unit = $4, min_safe = $5, max_safe = $6 where id = $1`
-	res, err := m.db.ExecContext(ctx, query, s.ID, s.SiteID, s.Name, s.Unit, s.MinSafe, s.MaxSafe)
+
+	err := m.exec(ctx, "update-record", query, func() error {
+		res, err := m.db.ExecContext(ctx, query, s.ID, s.SiteID, s.Name, s.Unit, s.MinSafe, s.MaxSafe)
+		if err != nil {
+			return err
+		}
+
+		n, err = res.RowsAffected()
+		return err
+	})
+
 	if err != nil {
-		level.Error(m.logger).Log("message", err.Error())
 		return 0, err
 	}
-
-	span.SetTag("db.type", "sql")
-	span.LogFields(
-		opentracingLog.String("event", "update-record"),
-		opentracingLog.String("db.statement", query),
-	)
-
-	n, err := res.RowsAffected()
-	if err != nil {
-		level.Error(m.logger).Log("message", err.Error())
-		return 0, err
-	}
-
 	return int(n), nil
 }
 
 func (m *postgresSensorManager) Delete(ctx context.Context, id string) error {
-	parentSpan := opentracing.SpanFromContext(ctx)
-	span := m.tracer.StartSpan("delete-record", opentracing.ChildOf(parentSpan.Context()))
-	defer span.Finish()
-
 	query := `delete from sensors where id = $1`
-	_, err := m.db.ExecContext(ctx, query, id)
-	if err != nil {
-		level.Error(m.logger).Log("message", err.Error())
+
+	err := m.exec(ctx, "delete-record", query, func() error {
+		_, err := m.db.ExecContext(ctx, query, id)
 		return err
-	}
+	})
 
-	span.SetTag("db.type", "sql")
-	span.LogFields(
-		opentracingLog.String("event", "delete-record"),
-		opentracingLog.String("db.statement", query),
-	)
-
-	return nil
+	return err
 }
