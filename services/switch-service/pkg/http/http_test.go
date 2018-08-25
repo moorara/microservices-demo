fb05d0ba83025b4aa8298e384e8a1c0864e3f0e2
Milad Irannejad <moorara@users.noreply.github.com>
2018-08-25 10:42:56 -0400

    switch-service: renaming a test (#81)

--- services/switch-service/pkg/http/http_test.go
+++ services/switch-service/pkg/http/http_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test(t *testing.T) {
+func TestResponseWriter(t *testing.T) {
 	tests := []struct {
 		name        string
 		statusCode  int
