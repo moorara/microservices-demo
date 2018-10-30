c830e5d97e07377062a279442ce896146c640b73
Milad Irannejad <moorara@users.noreply.github.com>
2018-10-29 22:32:17 -0400

    sensor-service: refactor postgres configs (#115)

--- services/sensor-service/service/sensor_test.go
+++ services/sensor-service/service/sensor_test.go
@@ -22,22 +22,39 @@ func CreateContextWithSpan() context.Context {
 
 func TestNewSensorManager(t *testing.T) {
 	tests := []struct {
-		name        string
-		postgresURL string
+		name     string
+		host     string
+		port     string
+		database string
+		username string
+		password string
 	}{
 		{
-			"WithoutUserPass",
-			"postgres://localhost",
+			name:     "Simple",
+			host:     "localhost",
+			port:     "5432",
+			database: "store",
 		},
 		{
-			"WithUserPass",
-			"postgres://root:pass@localhost",
+			name:     "WithUsername",
+			host:     "localhost",
+			port:     "5432",
+			database: "store",
+			username: "root",
+		},
+		{
+			name:     "WithPassword",
+			host:     "localhost",
+			port:     "5432",
+			database: "store",
+			username: "root",
+			password: "pass",
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			db := NewPostgresDB(tc.postgresURL)
+			db := NewPostgresDB(tc.host, tc.port, tc.database, tc.username, tc.password)
 			logger := log.NewNopLogger()
 			tracer := mocktracer.New()
 			m := NewSensorManager(db, logger, tracer)
