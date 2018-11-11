bab45e39213d1f5a250780d5ee13259116cc4233
Milad Irannejad <moorara@users.noreply.github.com>
2018-11-10 20:15:54 -0500

    Move out api-test util (#129)

--- tests/api-test/internal/service/graphql.go
+++
@@ -1,9 +0,0 @@
-package service
-
-type (
-	// GraphQLTest is for testing a service with GraphQL API
-	GraphQLTest struct {
-		Name           string `yaml:"name"`
-		ServiceAddress string `yaml:"service_address"`
-	}
-)
