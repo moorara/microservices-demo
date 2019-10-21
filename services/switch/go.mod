00bef09aa1e86ab8d64bbedb12f1bc698ce1adcc
Milad Irannejad <moorara@gmail.com>
2019-10-20 22:49:07 -0400

    Rename switch-service to switch

---
+++ services/switch/go.mod
@@ -0,0 +1,22 @@
+module github.com/moorara/microservices-demo/services/switch
+
+go 1.13
+
+require (
+	github.com/arangodb/go-driver v0.0.0-20191016162805-64c417a99f5f
+	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
+	github.com/go-kit/kit v0.9.0
+	github.com/golang/protobuf v1.3.2
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/mux v1.7.3
+	github.com/moorara/konfig v0.3.2
+	github.com/opentracing/opentracing-go v1.1.0
+	github.com/prometheus/client_golang v1.2.1
+	github.com/stretchr/testify v1.4.0
+	github.com/uber-go/atomic v1.4.0 // indirect
+	github.com/uber/jaeger-client-go v2.19.0+incompatible
+	github.com/uber/jaeger-lib v2.2.0+incompatible
+	go.uber.org/atomic v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20191014212845-da9a3fd4c582
+	google.golang.org/grpc v1.24.0
+)
---
+++ services/switch/pkg/trace/tracer.go
@@ -0,0 +1,95 @@
+package trace
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/prometheus/client_golang/prometheus"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	jaeger "github.com/uber/jaeger-client-go"
+	jaegerConfig "github.com/uber/jaeger-client-go/config"
+	jaegerMetrics "github.com/uber/jaeger-lib/metrics"
+	jaegerPrometheus "github.com/uber/jaeger-lib/metrics/prometheus"
+)
+
+// jaegerLogger implements jaeger.Logger
+type jaegerLogger struct {
+	logger log.Logger
+}
+
+func (l *jaegerLogger) Error(msg string) {
+	_ = level.Error(l.logger).Log("message", msg)
+}
+
+func (l *jaegerLogger) Infof(msg string, args ...interface{}) {
+	_ = level.Info(l.logger).Log("message", fmt.Sprintf(msg, args...))
+}
+
+// NewConstSampler creates a constant Jaeger sampler
+func NewConstSampler() *jaegerConfig.SamplerConfig {
+	return &jaegerConfig.SamplerConfig{
+		Type:  "const",
+		Param: 1,
+	}
+}
+
+// NewProbabilisticSampler creates a probabilistic Jaeger sampler
+func NewProbabilisticSampler(probability float64) *jaegerConfig.SamplerConfig {
+	return &jaegerConfig.SamplerConfig{
+		Type:  "probabilistic",
+		Param: probability,
+	}
+}
+
+// NewRateLimitingSampler creates a rate limiting Jaeger sampler
+func NewRateLimitingSampler(spansPerSecond float64) *jaegerConfig.SamplerConfig {
+	return &jaegerConfig.SamplerConfig{
+		Type:  "rateLimiting",
+		Param: spansPerSecond,
+	}
+}
+
+// NewReporter creates a Jaeger reporter
+func NewReporter(logSpans bool, jaegerAgentAddr string) *jaegerConfig.ReporterConfig {
+	return &jaegerConfig.ReporterConfig{
+		LogSpans:           logSpans,
+		LocalAgentHostPort: jaegerAgentAddr,
+	}
+}
+
+// NewTracer creates a new tracer
+func NewTracer(serviceName string, sampler *jaegerConfig.SamplerConfig, reporter *jaegerConfig.ReporterConfig, logger log.Logger, registerer prometheus.Registerer) (opentracing.Tracer, io.Closer) {
+	jgConfig := &jaegerConfig.Configuration{
+		ServiceName: serviceName,
+		Sampler:     sampler,
+		Reporter:    reporter,
+	}
+
+	var jglogger jaeger.Logger
+	if logger == nil {
+		jglogger = jaeger.NullLogger
+	} else {
+		jglogger = &jaegerLogger{logger}
+	}
+	loggerOption := jaegerConfig.Logger(jglogger)
+
+	var metricsFactory jaegerMetrics.Factory
+	if registerer == nil {
+		metricsFactory = jaegerMetrics.NullFactory
+	} else {
+		registererOption := jaegerPrometheus.WithRegisterer(registerer)
+		metricsFactory = jaegerPrometheus.New(registererOption)
+		metricsFactory = metricsFactory.Namespace(jaegerMetrics.NSOptions{Name: serviceName})
+	}
+	metricsOption := jaegerConfig.Metrics(metricsFactory)
+
+	jaegerTracer, closer, err := jgConfig.NewTracer(loggerOption, metricsOption)
+	if err != nil {
+		panic(err)
+	}
+
+	return jaegerTracer, closer
+}
